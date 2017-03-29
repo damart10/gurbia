60235fafe80739177aeca9559ea465266ef262f0
Daniel Martínez <dmartinezmont@gmail.com>
2017-03-29 00:03:19 -0500

    Estructura básica del proyecto

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,6 +1,25 @@
-export function getUser(data) {
-  return {
+import Firebase from 'firebase'
+
+const db = new Firebase('https://gurbia-1330f.firebaseio.com/');
+
+export function getUser() {
+  return dispatch => {
+    db.on('value', snapshot => {
+      dispatch({
+        type: 'GET_USER',
+        payload: snapshot.val()
+      });
+    });
+  };return {
     type: 'GET_USER',
     payload: data
   }
+}
+
+export function addUser(data) {
+  return dispatch => db.push(data);
+}
+
+export function deleteUser(key) {
+  return dispatch => db.child(key).remove();
 }
