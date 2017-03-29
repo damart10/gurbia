1f2caf0dc3220c966d240d8f976c19fc419c19e4
Daniel Martínez <dmartinezmont@gmail.com>
2017-03-28 23:54:07 -0500

    Estructura básica del proyecto

--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { AppRegistry } from 'react-native'
+import { Provider } from 'react-redux'
 
 import App from './src/App'
+import store from './src/store'
 
 export default class Gurbia extends Component {
   render() {
     return(
-      <App initialRoute={{id: 'Login'}} />
+      <Provider store={store}>
+        <App initialRoute={{id: 'Login'}} />
+      </Provider>
     )
   }
 }
